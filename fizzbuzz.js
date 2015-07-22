4ad8c77e0bc434bc599488785de7e7523dffddeb
Andrew Premdas <apremdas@yahoo.co.uk>
2015-07-22 14:28:29 +0100

    fizzbuzz enhanced

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,11 +1,13 @@
-var fizz = 'Fizz', buzz = 'Buzz'
+var fizz = 'Fizz', buzz = 'Buzz', fizzbuzz= 'FizzBuzz'
 
 
-for(i=0; i<19; i++){
+for(i=0; i<100; i++){
   isfizz = (i%3 == 0);
   isbuzz = (i%5 == 0);
 
-  if (isbuzz && !isfizz)
+  if (isbuzz && isfizz)
+    console.log(fizzbuzz)
+  else if (isbuzz)
     console.log(buzz);
   else if (isfizz)
     console.log(fizz);
