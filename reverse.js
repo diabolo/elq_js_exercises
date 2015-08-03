6922269eb98359f16d0e93e01af7e12e813d5fa5
Andrew Premdas <apremdas@yahoo.co.uk>
2015-08-03 23:45:13 +0100

    reverseArrayInPlace
    - extended test cases and shared them.
    - implemented in place reverse by swapping.

--- reverse.js
+++ reverse.js
@@ -8,3 +8,14 @@ exports.reverseArray = function(arr) {
 
   return res;
 }
+
+exports.reverseArrayInPlace = function(arr) {
+  stop = Math.floor(arr.length/2)
+  for(var i=arr.length-1, j=0; i>=stop; i--,j++) {
+    tmp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = tmp;
+  }
+
+  return arr;
+}
--- test/reverse.js
+++ test/reverse.js
@@ -1,16 +1,32 @@
 var expect = require('chai').expect;
 var util  = require("../reverse.js")
 
-describe('reverseArray', function() {
-  var tests = [
-    { args: [1,2], expected: [2,1] },
-    { args: [], expected: [] },
-    { args: [1,1,2,3,5], expected: [5,3,2,1,1] },
-  ];
+var tests = [
+  { args: [1,2], expected: [2,1] },
+  { args: [], expected: [] },
+  { args: [1], expected: [1] },
+  { args: [1,2,3], expected: [3,2,1] },
+  { args: [1,2,3,4], expected: [4,3,2,1] },
+  { args: [1,1,2,3,5], expected: [5,3,2,1,1] },
+];
 
+describe('reverseArray', function() {
   tests.forEach(function(test) {
     it('reverses ' + test.args + ' to ' + test.expected, function() {
       expect(util.reverseArray(test.args)).to.eql(test.expected);
+    });
+  });
+});
+
+describe('reverseArrayInPlace', function() {
+  it('changes the original array', function() {
+    arr = [];
+    expect(util.reverseArrayInPlace(arr)).to.equal(arr);
+  });
+
+  tests.forEach(function(test) {
+    it('reverses ' + test.args + ' to ' + test.expected, function() {
+      expect(util.reverseArrayInPlace(test.args)).to.eql(test.expected);
     });
   });
 });
