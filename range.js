2219379399689c0adf387d32af4e821f703b2da4
Andrew Premdas <apremdas@yahoo.co.uk>
2015-08-03 21:42:31 +0100

    Range dealing with -ve and 0 inc values

--- range.js
+++ range.js
@@ -1,11 +1,26 @@
 exports = module.exports = {}
 
-exports.range = function(start, end) {
+exports.range = function(start, end, inc) {
   var res = [];
+  inc = typeof inc !== 'undefined' ? inc : 1;
+  
+  // what if inc is 0 ?
 
-  for(var i=start; i <= end; i+=1) {
-    res.push(i);
-  };
+  if (inc > 0)
+    for(var i=start; i <= end; i+=inc)
+      res.push(i);
+  else if (inc < 0) {
+    console.log(
+        'start: ' + start +
+        ' end: ' + end +
+        ' i: ' + i +
+        ' res: ' + res
+        );
+    for(var i=start; i >= end; i+=inc)
+      res.push(i);
+  }
+  else
+    ; // do nowt and return []
 
   return res;
 };
--- test/range.js
+++ test/range.js
@@ -4,6 +4,8 @@ var util  = require("../range.js")
 describe('range', function() {
   var tests = [
     { args: [1,3], expected: [1,2,3]},
+    { args: [-1,2], expected: [-1,0,1,2]},
+    { args: [3,0,-1], expected: [3,2,1,0]},
   ];
 
   tests.forEach(function(test) {
