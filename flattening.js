b64fe10062877bb14b82296797214eeef56a882b
Andrew Premdas <apremdas@yahoo.co.uk>
2015-08-06 17:28:03 +0100

    Working flattener:
    This is recursive and flattens any depth of arrays (stack size
    allowing). I'm not convinced its idiomatic javascript but hey its a
    start.

--- flattening.js
+++ flattening.js
@@ -1,5 +1,22 @@
 exports = module.exports = {}
 
+function _flat(arr) {
+  return arr.reduce(reducer,[]);
+}
+
+function reducer(prev, curr, idx, arr) {
+  console.log('reducer ' + 
+   ' prev: ' + JSON.stringify(prev) +
+   ' curr: ' + JSON.stringify(curr) +
+   ' idx: ' + JSON.stringify(idx) +
+   ' arr: ' + JSON.stringify(arr)
+   );
+  if (curr instanceof Array)
+    return prev.concat(_flat(curr));
+  else
+    return prev.concat(curr);
+}
+
 exports.flatten = function(arr) {
-  return [];
+  return _flat(arr);
 }
--- test/flattening.js
+++ test/flattening.js
@@ -2,12 +2,17 @@ var expect = require('chai').expect;
 var util = require('../flattening.js')
 
 var tests = [
-  { arg: [[1,2][3,4]], expected: [1,2,3,4] },
+  { arg: [[1,2],[3,4]], expected: [1,2,3,4] },
+  { arg: [[1,2],[3, [4,5]]], expected: [1,2,3,4,5] },
 ]
+
+function arToS(arr) {
+  return JSON.stringify(arr);
+}
 
 describe('flattening', function () {
   tests.forEach(function(test) {
-    it('flattens ' + test.arg + ' to ' + test.expected, function() {
+    it('flattens ' + arToS(test.arg) + ' to ' + test.expected, function() {
       expect(util.flatten(test.arg)).to.eql(test.expected);
     });
   });
