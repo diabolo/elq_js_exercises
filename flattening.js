e51743b69f840dd67a0cbcca911a5e0fde8674ed
Andrew Premdas <apremdas@yahoo.co.uk>
2015-08-06 17:34:31 +0100

    Inlined reducer and removed logging, now idiomatic.

--- flattening.js
+++ flattening.js
@@ -1,20 +1,12 @@
 exports = module.exports = {}
 
 function _flat(arr) {
-  return arr.reduce(reducer,[]);
-}
-
-function reducer(prev, curr, idx, arr) {
-  console.log('reducer ' + 
-   ' prev: ' + JSON.stringify(prev) +
-   ' curr: ' + JSON.stringify(curr) +
-   ' idx: ' + JSON.stringify(idx) +
-   ' arr: ' + JSON.stringify(arr)
-   );
-  if (curr instanceof Array)
-    return prev.concat(_flat(curr));
-  else
-    return prev.concat(curr);
+  return arr.reduce(function(prev, curr) {
+    if (curr instanceof Array)
+      return prev.concat(_flat(curr));
+    else
+      return prev.concat(curr);
+  },[]);
 }
 
 exports.flatten = function(arr) {
